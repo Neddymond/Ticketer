e42bd0bf1b2682395bdb348c51dce5021b8bb4fd
Chinedu Ikechi <chineduikechi@Tech-Chinedu-2.local>
2022-06-27 01:09:27 +0100

    Set up test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,30 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import { startMongo } from './config/mongodb';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({  signed: false, secure: true }));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', () => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
+import { app } from './app';
 
 app.listen(4000, () => console.log('server is listening on port 4000!!!'));
 
